5dd6d935deab94a676affbc062c6bf4d35bc1378
Kevin Middleton <middletonk@missouri.edu>
2024-02-25 20:09:28 -0600

    Init CT files

---
+++ CT/NHANES/NHANES_Setup.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+NH_demo <- foreign::read.xport("P_DEMO.XPT") |> 
+  select(SEQN, RIAGENDR, RIDAGEYR) |> 
+  mutate(Sex = if_else(RIAGENDR == 1, "XY Male", "XX Female")) |> 
+  rename(Age = RIDAGEYR)
+  
+NH_data <- foreign::read.xport("P_BMX.XPT") |> 
+  select(SEQN, BMXWT, BMXHT) |> 
+  rename(Weight = BMXWT,
+         Height = BMXHT)
+
+NH <- left_join(NH_demo, NH_data, by = join_by(SEQN)) |> 
+  drop_na() |> 
+  select(-SEQN, -RIAGENDR) |> 
+  as_tibble()
+
+write_rds(NH, file = "NHANES.Rds")
+
